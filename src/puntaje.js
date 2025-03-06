8167aa64f35ab7416cdc401f0ee81d37c2455a75
andresrev070303 <andres.revollo@ucb.edu.bo>
2025-03-06 09:42:41 -0400

    feat:Devuelve 30-0 si el puntaje es 2-0

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,7 +1,9 @@
 function obtenerScore(score1, score2) {
   if ( score1 === 1 && score2 === 0) {
     return "15-0";
-  }
+  }else if (score1 === 2 && score2 === 0) {
+    return "30-0";
+  }  
   else return "Love-Love";
 }
 
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -7,6 +7,9 @@ describe("Puntaje", () => {
   it("Deberia devolver 15-0 si el puntaje es 1-0", () => {
     expect(obtenerScore(1, 0)).toEqual("15-0");
   });
+  it("Deberia devolver 30-0 si el puntaje es 2-0", () => {
+    expect(obtenerScore(2, 0)).toEqual("30-0");
+  });
 });
 
 
